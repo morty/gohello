02b34ae65e19268bcfba5ed2f981e6dab61e1bfe
Tom Mortimer-Jones <tom@morty.co.uk>
2024-08-19 15:02:20 +0100

    Subdirectory

---
+++ hello2/go.mod
@@ -0,0 +1,3 @@
+module github.com/morty/gohello/hello2
+
+go 1.22.1
---
+++ hello2/main.go
@@ -0,0 +1,7 @@
+package hello2
+
+import "fmt"
+
+func HelloAgain() {
+	fmt.Println("Hello from github.com/morty/gohello/hello2")
+}
